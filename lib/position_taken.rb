05fcfbd1718fa8e1b786d943864a17dec33f6bae
github username <github email address>
2019-08-08 18:04:35 +0100

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+taken = nil
+if (board[index] == " " || board[index] == "" || board[index] == nil)
+	taken = false
+else
+	taken = true
+end
+
+taken #why is there any extra taken? and an extra end?
+
+end
